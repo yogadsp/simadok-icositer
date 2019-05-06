5894e0dba49ffb61b423db6edf7c84e81d7be86e
Mahesa Darma Satria <mahesadarmasatria@gmail.com>
2019-05-06 14:46:34 +0700

    edit user
    memperbaiki tampilan di bagian edit user

--- application/views/admin/edit-user.php
+++ application/views/admin/edit-user.php
@@ -9,29 +9,31 @@
     <link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap.min.css">
     
     <link rel="stylesheet" href="<?php echo base_url();?>assets/jquery-ui/jquery-ui.min.css">
-    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/login.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/data.css">
 </head>
 <body>
 <?php $this->load->view('admin/header_adit'); ?>
 
 <form action="<?php echo base_url('data_user/updateUser') ?>" method="post">
-        <center>
-            <h3>EDIT USER</h3>
-
+    <center>    
+    <div class="avatar">
+      <img src="<?php echo base_url();?>assets/img/userr.png" alt="Avatar" width="150">
+    </div>
+        
             <p>
-                <label>ID User    </label>
+                <label style="padding-right: 370px;"><b>Username</b></label>
                     <input type="hidden" name="user_lama" value="<?php echo $user->id_user ?>">
-                    <input type="text" name="user" id="user" size="40" value ="<?php echo $user->id_user ?>" required>
+                    <input class="form-control col-md-4" type="text" name="user" id="user" size="40" value ="<?php echo $user->id_user ?>" required>
             </p>
             <p>
-                <label>Password    </label>
-                    <input type="text" name="pass" id="pass" size="40" value ="<?php echo $user->pass ?>" required>
+                <label style="padding-right: 370px;"><b>Password</b>    </label>
+                    <input class="form-control col-md-4" type="text" name="pass" id="pass" size="40" value ="<?php echo $user->pass ?>" required>
             </p>
             <p>
-                <label>Peran    :</label>
+                <label style="padding-right: 375px;"><b>Kategori</b></label>
                     <!-- mengulang data berdasarkan data yang telah diambil dari controller -->
-                    <input type="hidden" name="peran_lama" value="<?php echo $user->id_peran ?>">
-                    <select id="peran" name="peran">
+                    <input  type="hidden" name="peran_lama" value="<?php echo $user->id_peran ?>">
+                    <select  class="form-control col-md-4" id="peran" name="peran">
                         <?php foreach ($peran->result() as $row ) { ?>
                             <option value="<?php echo $row->id_peran?>" <?php echo ($user->id_peran == $row->id_peran) ? 'selected' : '' ?>>
                                 <?php echo $row->nama_peran;?>
@@ -39,7 +41,7 @@
                         <?php } ?>
                     </select>
             </p>
-            <input type="submit" name="submitDataa" id="submitDataa" value="SUBMIT">
+            <input class="btn btn-success" type="submit" name="submitDataa" id="submitDataa" value="SUBMIT">
         </center>
     </form>
 
