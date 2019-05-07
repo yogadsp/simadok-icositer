5887cb7cfc6e172e3297550cd8732b0f9a07cfae
yogadsp <yogaseptana@gmail.com>
2019-05-07 12:08:19 +0700

    Update icositer.sql

--- icositer.sql
+++ icositer.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 05 Mei 2019 pada 10.12
+-- Generation Time: 07 Mei 2019 pada 12.07
 -- Versi Server: 10.1.29-MariaDB
 -- PHP Version: 7.2.0
 
@@ -40,8 +40,7 @@ CREATE TABLE `bidang` (
 INSERT INTO `bidang` (`id_bidang`, `nama_bidang`) VALUES
 (1, 'JSAT'),
 (11, 'IOP'),
-(21, 'sbsn'),
-(22, 'ADAS');
+(21, 'sbsn');
 
 -- --------------------------------------------------------
 
@@ -60,11 +59,7 @@ CREATE TABLE `bid_subbid` (
 --
 
 INSERT INTO `bid_subbid` (`id_bid_subbid`, `id_bidang`, `id_subbidang`) VALUES
-(29, 1, 34),
-(30, 1, 35),
-(31, 11, 36),
-(32, 11, 37),
-(33, 21, 38);
+(34, 1, 39);
 
 -- --------------------------------------------------------
 
@@ -79,14 +74,6 @@ CREATE TABLE `galeri` (
   `gambar` varchar(50) NOT NULL,
   `id_user` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data untuk tabel `galeri`
---
-
-INSERT INTO `galeri` (`id_galeri`, `tgl_galeri`, `keterangan`, `gambar`, `id_user`) VALUES
-(6, '2019-05-08', 'Pembagian Hadiah', 'b9bd332a7c109d857208efa67c551833.png', 'yoga'),
-(7, '2019-05-15', 'Pembagian', 'c1f16a8141d56539f3c3af00c7a9fa78.jpg', 'yoga');
 
 -- --------------------------------------------------------
 
@@ -104,22 +91,8 @@ CREATE TABLE `jurnal` (
 --
 
 INSERT INTO `jurnal` (`id_jurnal`, `judul_jurnal`) VALUES
-(1, 'The Delay Simulation on Hierarchical Structure for Semi-Double Track of Railway Line Using Max-Plus Algebra'),
-(2, 'A systematic survey of plant biodiversity study within the land of Institut Teknologi Sumatera (ITERA)'),
-(3, 'Development of Submerged Biofilter Design for Wastewater Conservation: Carbonaceous Removal Study'),
-(4, 'Design of Ontology-based Question Answering System for Incompleted Sentence Problem'),
-(5, 'The effect of cross-shaped line width on the absorbance performance of terahertz metamaterial based on paper as spacer'),
-(6, 'Atmospheric drag effect on LAPAN A1 orbit during geomagnetic storm 2017'),
-(98, 'Perubahan Lingkungan Hidup'),
-(99, 'Keamanan Data'),
-(100, 'Penanaman Kembali'),
-(101, 'Dampak Lingkungan'),
-(102, 'Perubahan Ekonomi'),
-(103, 'Orang- Orang'),
-(104, 'Ini Judul Jurnal'),
-(105, 'Perubahan Bidang'),
-(106, 'ASDD'),
-(107, 'Kehutanan Hujan');
+(108, 'Ini Jdullllll'),
+(109, 'asd');
 
 -- --------------------------------------------------------
 
@@ -165,19 +138,7 @@ CREATE TABLE `pusat_data` (
 --
 
 INSERT INTO `pusat_data` (`id_pusat`, `tgl_input`, `id_user`, `id_jurnal`, `id_bid_subbid`, `nama_penulis`, `email`, `afiliasi`, `status`, `dokumen`) VALUES
-(15, '2019-05-01', 'yoga', 101, 31, 'ASEEQQ', 'andi@gmal.comm', 'ITERA', 'Sudah Terupload', ''),
-(16, '2019-05-01', 'yoga', 102, 30, 'Yoga Dwi Septana, Alafadi Pratama', 'yoga@gmail.com', 'ITERA', 'Sudah Terupload', 'b2348924d70581c5525552be456dd407.pdf'),
-(17, '2019-05-01', 'yoga', 103, 33, 'Joko, Adi', 'adi@gmail.com', 'ITERA', 'Pilihan', '7ef40d9e353b4bb34cf58544ca60e1cb.pdf'),
-(18, '2019-05-01', 'yoga', 104, 33, 'Ini Nama, Ini Juga', 'ini@email.com', 'INI', 'Ini Status', 'b685b1378a22d506db15ca5b1c641d55.pdf'),
-(19, '2019-05-01', 'yoga', 105, 33, 'Orang', 'orang@gmail.com', 'ITERA', 'Masih Jurnal', 'ee267297895d67416961d87147d28d4c.pdf'),
-(20, '2020-09-01', 'yoga', 2, 29, 'Yoga', 'yoga@gmail.com', 'ITERA', 'Mahasiswa', 'qweqweqdasdqwdqw.pdf'),
-(21, '2021-01-09', 'yoga', 6, 33, 'Kaka', 'kaka@gmail.com', 'ITERA', 'Jarang', 'asdasl;qhwleknqwe.pdf'),
-(22, '2019-05-09', 'yoga', 3, 30, 'Golang', 'golang@gmail.com', 'ITERA', 'Sendiri', 'alskfjalkfadfdf.pdf'),
-(23, '2019-05-01', 'yoga', 1, 30, 'Dea', 'dea@gmail.com', 'ITERA', 'Sendiri', 'laksflakjfsdf.pdf'),
-(24, '2019-05-08', 'yoga', 100, 30, 'Kokok', 'koko@gmail.com', 'ITB', 'Terupload', 'lkasjdlkajsldkjasd.php'),
-(25, '2019-05-06', 'yoga', 98, 32, 'Jaim', 'jaim@gmail.com', 'ITS', 'Rame', 'asldalsjdasd.pdf'),
-(26, '2019-05-02', 'yoga', 106, 33, 'JJA', 'jja@gmail.com', 'ITB', 'Sendiri', '0322ddd797a93e4a7f1d1bc08141d397.pdf'),
-(27, '2019-05-04', 'yoga', 107, 31, 'Andi, Dono', 'dono@gmail.com', 'ITERA', 'Sudah Terupload', '22bd4c1f08d2dffc1a68b5ba31be2bb3.pdf');
+(29, '2019-05-07', 'yoga', 109, 34, 'asdas', 'asdasd', 'as', 'asd', '36f6c809103f74b338120ed68804ac7e.pdf');
 
 -- --------------------------------------------------------
 
@@ -195,21 +156,7 @@ CREATE TABLE `sub_bidang` (
 --
 
 INSERT INTO `sub_bidang` (`id_subbidang`, `nama_subbidang`) VALUES
-(1, 'Science and Applicative Technology'),
-(2, 'Environmental Science and Sustainable Development'),
-(3, 'Fundamental Science'),
-(4, 'Agricultural Technology and Smart Farm'),
-(5, 'Atmospheric Science'),
-(6, 'Energy System'),
-(7, 'General'),
-(8, 'Information and Communication Technology (ICT)'),
-(9, 'Smart and Advanced Materials'),
-(10, 'Smart and Green Infstructurera'),
-(34, 'Lingkungan'),
-(35, 'Keamanan'),
-(36, 'Kehutanan'),
-(37, 'Ekonomi'),
-(38, 'Ini Bidang');
+(39, 'Contoh');
 
 -- --------------------------------------------------------
 
@@ -228,10 +175,9 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id_user`, `pass`, `id_peran`) VALUES
-('haha', 'e9f5713dec55d727bb35392cec6190ce', 3),
-('hoh', '614b7df69165603557df83f9af9dc02a', 2),
 ('yoga', '21232f297a57a5a743894a0e4a801fc3', 1),
-('yoga', '5aee9dbd2a188839105073571bee1b1f', 2);
+('yoga', '5aee9dbd2a188839105073571bee1b1f', 2),
+('yoga', 'de3709b8e6f81a4ef5a858b7a2d28883', 3);
 
 --
 -- Indexes for dumped tables
@@ -306,7 +252,7 @@ ALTER TABLE `bidang`
 -- AUTO_INCREMENT for table `bid_subbid`
 --
 ALTER TABLE `bid_subbid`
-  MODIFY `id_bid_subbid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `id_bid_subbid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 
 --
 -- AUTO_INCREMENT for table `galeri`
@@ -318,19 +264,19 @@ ALTER TABLE `galeri`
 -- AUTO_INCREMENT for table `jurnal`
 --
 ALTER TABLE `jurnal`
-  MODIFY `id_jurnal` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=108;
+  MODIFY `id_jurnal` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=110;
 
 --
 -- AUTO_INCREMENT for table `pusat_data`
 --
 ALTER TABLE `pusat_data`
-  MODIFY `id_pusat` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `id_pusat` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT for table `sub_bidang`
 --
 ALTER TABLE `sub_bidang`
-  MODIFY `id_subbidang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+  MODIFY `id_subbidang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
