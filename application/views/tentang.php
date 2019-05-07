b815c961c212c07c8956182b483377d21b344e20
Mahesa Darma Satria <mahesadarmasatria@gmail.com>
2019-05-07 08:39:33 +0700

    tentang
    membuat tampilan tentang

--- application/views/tentang.php
+++ application/views/tentang.php
@@ -6,18 +6,54 @@
     <title>Tentang</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="<?php echo base_url();?>assets/css/login.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/galery.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/dash.css">
 </head>
 <body>
     <?php $this->load->view('header_user'); ?>
 
     <center>
-        <h2>TENTANG</h2>
+        <br><br>
+        <h1><b>TENTANG</b></h1>
+        <div class="col-md-9" style="text-align:justify;">
+        <p><h6>
+           International Conference on Science,
+Infrastucture, Technology and Regional Development
+Konferensi internasional yang menghadirkan tokoh
+sains, infrastruktur, dan pengembangan wilayah
+yang dilakukan setiap tahun untuk merayakan Dies Natalis
+Institut Teknologi Sumatera
+        </h6></p></div>
 
-        <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-            Consequuntur sit aliquam dolor ad quia officiis! Recusandae quae, 
-            optio adipisci nihil quod aperiam fugit, quasi dolorem ipsa mollitia quia provident quibusdam.
-        </p>
+<!-- data pembuat web -->
+<div class="gallery">
+  <a target="_blank" href="img_5terre.jpg">
+    <img src="img_5terre.jpg" alt="Cinque Terre" width="600" height="400">
+  </a>
+  <div class="desc">Mahesa Darma Satria</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="img_forest.jpg">
+    <img src="img_forest.jpg" alt="Forest" width="600" height="400">
+  </a>
+  <div class="desc">Yoga Dwi Septana</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="img_lights.jpg">
+    <img src="img_lights.jpg" alt="Northern Lights" width="600" height="400">
+  </a>
+  <div class="desc">Fardiansyah</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="img_mountains.jpg">
+    <img src="img_mountains.jpg" alt="Mountains" width="600" height="400">
+  </a>
+  <div class="desc">Bayu Winnie</div>
+</div>
 
     </center>
 
