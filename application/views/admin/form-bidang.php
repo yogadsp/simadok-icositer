0701177b3fea4492c497b5aebad0697afd4f493e
Mahesa Darma Satria <mahesadarmasatria@gmail.com>
2019-05-10 09:08:07 +0700

    form-bidang
    color

--- application/views/admin/form-bidang.php
+++ application/views/admin/form-bidang.php
@@ -3,7 +3,7 @@
 						  <div class="modal-dialog modal-dialog-centered" role="document">
 						    <div class="modal-content">
 						      <div class="modal-header">
-						        <h5 class="modal-title text-center" id="exampleModalLongTitle">TAMBAH BIDANG</h5>
+						        <h5 class="modal-title text-center" id="exampleModalLongTitle" style="color:brown;">TAMBAH BIDANG</h5>
 						        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
 						          <span aria-hidden="true">&times;</span>
 						        </button>
