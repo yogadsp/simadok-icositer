e14c3cdc796e0b12a29160323f647785304c48a6
Mahesa Darma Satria <mahesadarmasatria@gmail.com>
2019-05-14 12:23:15 +0700

    link linked

--- application/views/tentang.php
+++ application/views/tentang.php
@@ -34,10 +34,8 @@ Institut Teknologi Sumatera.
 
 <!-- data pembuat web -->
 <div class="gallery">
-  
-    <img src="<?php echo base_url();?>assets/img/mahesa.jpg" alt="Cinque Terre" width="600" height="400">
-  
-  <div class="desc">Mahesa Darma Satria<i style="color: blue;" class="fab fa-linkedin">&nbsp;</i><b><a target="_blank" href="https://www.linkedin.com/in/mahesa-darma-satria/">Linkedin</a></b></div>
+    <img src="<?php echo base_url();?>assets/img/mahesa.jpg" alt="Cinque Terre" width="500" height="400">
+      <div class="desc">Mahesa Darma Satria<br><i style="color: blue;" class="fab fa-linkedin">&nbsp;</i><b><a target="_blank" href="https://www.linkedin.com/in/mahesa-darma-satria/">Linkedin</a></b></div>
 </div>
 
 <div class="gallery">
@@ -50,27 +48,27 @@ Institut Teknologi Sumatera.
 
 <div class="gallery">
     <img src="<?php echo base_url();?>assets/img/fardiansyah.jpg" alt="Northern Lights" width="600" height="400">
-  <div class="desc">Fardiansyah Harahap<i style="color: blue;" class="fab fa-linkedin">&nbsp;</i><b><a target="_blank" href="https://www.linkedin.com/in/yogaseptana/">Linkedin</a></b></div>
+  <div class="desc">Fardiansyah Harahap<br><i style="color: blue;" class="fab fa-linkedin">&nbsp;</i><b><a target="_blank" href="https://www.linkedin.com/in/yogaseptana/">Linkedin</a></b></div>
 </div>
 
 <div class="gallery">
       <img src="<?php echo base_url();?>assets/img/winnie.jpg" alt="Mountains" width="600" height="400">
-    <div class="desc">Bayu Winnie<i style="color: blue;" class="fab fa-linkedin">&nbsp;</i><b><a target="_blank" href="https://www.linkedin.com/in/winnienapitupulu/">Linkedin</a></b></div>
+    <div class="desc">Bayu Winnie<br><i style="color: blue;" class="fab fa-linkedin">&nbsp;</i><b><a target="_blank" href="https://www.linkedin.com/in/winnienapitupulu">Linkedin</a></b></div>
 </div>
 
 <div class="gallery">
     <img src="<?php echo base_url();?>assets/img/rina.jpg" alt="Mountains" width="600" height="400">
-  <div class="desc">Rina Katrina<i style="color: blue;" class="fab fa-linkedin">&nbsp;</i><b><a target="_blank" href="https://www.linkedin.com/in/rina-naibah">Linkedin</a></b></div>
+  <div class="desc">Rina Katrina<br><i style="color: blue;" class="fab fa-linkedin">&nbsp;</i><b><a target="_blank" href="https://www.linkedin.com/in/rina-naibah">Linkedin</a></b></div>
 </div>
 
 <div class="gallery">
     <img src="<?php echo base_url();?>assets/img/pahsa.jpg" alt="Mountains" width="600" height="400">
-  <div class="desc">Pasha Abdul Khalid<i style="color: blue;" class="fab fa-linkedin">&nbsp;</i><b><a target="_blank" href="https://www.linkedin.com/in/pasha-abdul-khalid-969b80175">Linkedin</a></b></div>
+  <div class="desc">Pasha Abdul Khalid<br><i style="color: blue;" class="fab fa-linkedin">&nbsp;</i><b><a target="_blank" href="https://www.linkedin.com/in/pasha-abdul-khalid-969b80175">Linkedin</a></b></div>
 </div>
 
 <div class="gallery">
     <img src="<?php echo base_url();?>assets/img/user.png" alt="Mountains" width="600" height="400">
-  <div class="desc">Muhammad Agung<i style="color: blue;" class="fab fa-linkedin">&nbsp;</i><b><a target="_blank" href="https://www.linkedin.com/in/yogaseptana/">Linkedin</a></b></div>
+  <div class="desc">Muhammad Agung<br><i style="color: blue;" class="fab fa-linkedin">&nbsp;</i><b><a target="_blank" href="https://www.linkedin.com/in/yogaseptana/">Linkedin</a></b></div>
 </div>
 
     </center>
