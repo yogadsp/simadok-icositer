f0af937709fe7a059dfedb42fd7f8d086309c312
Mahesa Darma Satria <mahesadarmasatria@gmail.com>
2019-05-21 09:49:37 +0700

    tentang

--- application/views/tentang.php
+++ application/views/tentang.php
@@ -25,9 +25,72 @@ sains, infrastruktur, dan pengembangan wilayah
 yang dilakukan setiap tahun untuk merayakan Dies Natalis
 Institut Teknologi Sumatera.
         </h6></p>
+        
         <h6>
-           Web Dokumen Icositer ini dibangun oleh tim simadok_icositer yang berjumlah 7 orang, yang masing-masing memiliki kemampuan tersendiri dalam pengembangan web ini, dibawah ini profil singkat pengembang web dokumen icositer. 
-        </h6></p>
+          <b>
+          Struktur Perngurus Kegiatan Icositer :
+        </b>
+        </h6>
+        <br>
+        <p>
+        <h6>
+          <b>Penanggung Jawab :</b><br>
+          1. Prof. Ir. Ofyar Z. Tamin, M.Sc., Ph.D. <br>
+          2. Prof. Dr.-Ing. Mitra Djamal <br> 3. Prof. Dr. Sukrasno, M.S.
+        </h6>
+        <h6>
+          <b>Pengarah :</b><br>
+          1. Prof. Dr. Leo Hari Wiryanto, M.S.<br>
+          2. Dr. Rahayu Sulistyorini, M.T.
+        </h6>
+
+        <h6>
+          <b>Ketua :</b><br>
+          Prof. Dr. Toto Winata
+        </h6>
+        <h6>
+          <b>Wakil Ketua :</b><br>
+          Dr. Eng. Feerzet Achmad, M.T.
+        </h6>
+        <h6>
+          <b>Sekretaris :</b><br>
+          Monna Rozana, S.T., M.Phil., Ph.D.
+        </h6>
+        <h6>
+          <b>Bendahara :</b><br>
+          1. Dr.Sri Efrinita Irwan, M.Si.<br>
+          2. Rika Rosmalasari, A.Md.
+        </h6>
+
+        <h6>
+          <b>Editor :</b><br>
+          1. Hadi Teguh Yudistira, S.T., Ph.D.<br>
+          2. Dr. Eng. Lukman Nulhakim, S.Si., M.T.<br>
+          3. Nike Dwi Grevika Drantantiyas, S.Si., M.T.<br>
+          4. Dr. Sunarsih, S.S, M.A.<br>
+          5. Rifqi Ikhwanuddin, M.T.<br>
+        </h6>
+        <h6>
+          <b>Humas :</b><br>
+          1. Dr. Nono Agus Santoso, S.Si., M.T.<br>
+          2. Soni Satiawan, S.T., M.Sc.<br>
+          3. Zulfikar Adlan Nadzir, S.T., M.Sc.<br>
+        </h6>
+        <h6>
+          <b>Teknis dan Konsumsi :</b><br>
+          1. Dr.Sri Efrinita Irwan, M.Si.<br>
+          2. Rika Rosmalasari, A.Md.
+        </h6>
+        <h6>
+          <b>Sponsor :</b><br>
+          Hendra Saputra, S.TP., M.P.
+        </h6>
+      </p>
+
+
+
+
+      </p>
 
 
     </div>
